2b347bfa1e5dc180c23306479cd7a425cc5995f7
dan2dong <woozy345@gmail.com>
2022-10-16 04:14:22 +0900

    Feat: Inbox layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import "../styles/globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
