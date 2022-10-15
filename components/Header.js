7cef98e36a91677ddb49a29bd0ce004c57e322fe
wang-yurin <king_1994@naver.com>
2022-10-16 04:10:53 +0900

    Feat: header layout

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+const Header = () => {
+  return (
+    <Container>
+      <ArrowButton src="asset/icon.png" />
+      편지 작성
+      <VerticalRight></VerticalRight>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 55px;
+`;
+
+const ArrowButton = styled.img`
+  width: 34px;
+`;
+
+const VerticalRight = styled.div``;
+
+export default Header;
