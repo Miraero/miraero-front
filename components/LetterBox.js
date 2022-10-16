fafc66a4a115f9da5e1d15cd822d073ba9a5a10f
deipanema <miniharu.zip@gmail.com>
2022-10-16 10:20:20 +0900

    refactor: LetterBox component responsive web design

--- components/LetterBox.js
+++ components/LetterBox.js
@@ -2,88 +2,93 @@ import styled from "@emotion/styled";
 
 const LetterBox = () => {
   return (
-    <Letter>
+    <LetterContainer>
       <LetterImage src="asset/letter.png" />
       <LetterInfo>
         <LetterTitle>아이스크림 먹는 캄토노</LetterTitle>
-        <LetterTo>
+        <LetterToSection>
           <LetterToTitle>To</LetterToTitle>
           <LetterToDate>2022.10.16</LetterToDate>
-        </LetterTo>
-        <LetterFrom>
+        </LetterToSection>
+        <LetterFromSection>
           <LetterFromTitle>From</LetterFromTitle>
-          <LetterFromDate>2022.10.16</LetterFromDate>
-        </LetterFrom>
+          <LetterFromDate>2022.10.18</LetterFromDate>
+        </LetterFromSection>
       </LetterInfo>
-    </Letter>
+    </LetterContainer>
   );
 };
 
-const Letter = styled.div`
+const LetterContainer = styled.div`
+  width: 100%;
   display: flex;
   align-items: center;
-  width: 100%;
-  height: 100px;
-  border-radius: 1em;
-  margin-top: 10px;
-  padding: 0.2em 0;
+  border-radius: 0.6em;
+  margin-top: 0.8em;
+  padding: 1em 1em;
   box-shadow: var(--boxShadow);
+  cursor: pointer;
+
+  @media (max-width: 15rem) {
+    flex-direction: column;
+  }
 `;
 
 const LetterInfo = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  margin-right: 1em;
+  margin: 0.5em;
+  padding: 0.5em;
 `;
 
 const LetterImage = styled.img`
-  width: 150px;
-  height: 80px;
-  margin-left: 1em;
-  margin-right: 1em;
-  cursor: pointer;
+  max-width: 10em;
+  height: 5em;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const LetterTitle = styled.div`
-  margin-bottom: 20px;
-  font-weight: 500;
-  font-size: 16px;
+  margin-bottom: 1.5em;
+  font-weight: 600;
+  font-size: 0.9rem;
   color: var(--textColor);
 `;
 
-const LetterTo = styled.div`
+const LetterToSection = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
 const LetterToTitle = styled.div`
-  width: 50px;
-  font-size: 12px;
+  width: 3.5rem;
+  font-size: 0.7rem;
   font-weight: 600;
   color: var(--textColor);
 `;
 
 const LetterToDate = styled.div`
-  font-size: 12px;
+  flex: auto;
+  font-size: 0.7rem;
   color: var(--gray01);
 `;
-const LetterFrom = styled.div`
+
+const LetterFromSection = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
 const LetterFromTitle = styled.div`
-  width: 30px;
-  margin-right: 20px;
-  font-size: 12px;
+  width: 3.5rem;
+  font-size: 0.7rem;
   font-weight: 600;
   color: var(--textColor);
 `;
 
 const LetterFromDate = styled.div`
-  margin: 0;
-  font-size: 12px;
+  flex: auto;
+  font-size: 0.7rem;
   color: var(--gray01);
 `;
 
