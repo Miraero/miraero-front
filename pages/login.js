14b99f739d9f6a3041cf4501f1ded41158ef95b2
Seunghwan <ysp1307@naver.com>
2022-10-17 21:25:21 +0900

    feat: add headMeta

--- pages/login.js
+++ pages/login.js
@@ -2,81 +2,84 @@ import styled from "@emotion/styled";
 import client from "../lib/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-
+import HeadMeta from "../components/HeadMeta";
 const Login = () => {
-    useEffect(() => {
-        if (localStorage.getItem("accessToken")) {
-            router.push("/");
-        }
-    }, []);
-    const router = useRouter();
-    const handleLogin = (e) => {
-        e.preventDefault();
-        client.get("/login/oauth?code=1").then((res) => {
-            localStorage.setItem("name", res.data.nickName);
-            localStorage.setItem("accessToken", res.data.accessToken);
-            localStorage.setItem("refreshToken", res.data.refreshToken);
-            router.push("/");
-        });
-    };
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      router.push("/");
+    }
+  }, []);
+  const router = useRouter();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    client.get("/login/oauth?code=1").then((res) => {
+      localStorage.setItem("name", res.data.nickName);
+      localStorage.setItem("accessToken", res.data.accessToken);
+      localStorage.setItem("refreshToken", res.data.refreshToken);
+      router.push("/");
+    });
+  };
 
-    return (
-        <Container>
-            <Title>미래로 보내는 편지</Title>
-            <Align>
-                <Description>기억할 추억을 남기는 방법</Description>
-                <LoginBtn
-                    onClick={(e) => handleLogin(e)}
-                    src="asset/loginbutton.png"
-                />
-            </Align>
-        </Container>
-    );
+  return (
+    <>
+      <HeadMeta title="로그인" />
+      <Container>
+        <Title>미래로 보내는 편지</Title>
+        <Align>
+          <Description>기억할 추억을 남기는 방법</Description>
+          <LoginBtn
+            onClick={(e) => handleLogin(e)}
+            src="asset/loginbutton.png"
+          />
+        </Align>
+      </Container>
+    </>
+  );
 };
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    box-sizing: border-box;
-    width: 95vw;
-    height: 95vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+  width: 95vw;
+  height: 95vh;
 `;
 
 // 이미지 받으면 이미지로 변경 예정
 const Title = styled.h1`
-    width: 100px;
-    margin: 150px 0 0 0;
-    font-weight: normal;
-    font-family: "Noto Serif KR", serif;
-    &::after {
-        content: "";
-        display: inline-block;
-        position: relative;
-        bottom: 10px;
-        right: -2px;
-        width: 0.8em;
-        height: 0.1em;
-        background-color: black;
-        border-radius: 1em;
-    }
+  width: 100px;
+  margin: 150px 0 0 0;
+  font-weight: normal;
+  font-family: "Noto Serif KR", serif;
+  &::after {
+    content: "";
+    display: inline-block;
+    position: relative;
+    bottom: 10px;
+    right: -2px;
+    width: 0.8em;
+    height: 0.1em;
+    background-color: black;
+    border-radius: 1em;
+  }
 `;
 
 const Description = styled.p`
-    margin-top: 20px;
-    font-family: "Noto Serif KR", serif;
+  margin-top: 20px;
+  font-family: "Noto Serif KR", serif;
 `;
 
 const LoginBtn = styled.img`
-    width: 100%;
-    cursor: pointer;
+  width: 100%;
+  cursor: pointer;
 `;
 
 const Align = styled.div`
-    display: flex;
-    height: 100%;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 export default Login;
--- pages/write.js
+++ pages/write.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import {
-    DateText,
-    SmallFill,
-    PageWrapper,
-    LargeButton,
+  DateText,
+  SmallFill,
+  PageWrapper,
+  LargeButton,
 } from "../components/Atomic";
 import { Modal } from "../components/Modal";
 import styled from "@emotion/styled";
@@ -12,175 +12,179 @@ import { userDataState } from "../store/atom";
 import { useRecoilState } from "recoil";
 import client from "../lib/client";
 import { useRouter } from "next/router";
+import HeadMeta from "../components/HeadMeta";
 
 const Contents = styled.div`
-    margin-top: 102px;
+  margin-top: 102px;
 `;
 
 const InputTitle = styled.input`
-    padding: 10px 0px;
-    margin: 10px 0px;
-    border: none;
-    font-size: 24px;
-    border-bottom: 1px solid var(--gray02);
-    width: 100%;
-    outline: none;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    &:focus {
-        border-bottom: 1px solid black;
-    }
+  padding: 10px 0px;
+  margin: 10px 0px;
+  border: none;
+  font-size: 24px;
+  border-bottom: 1px solid var(--gray02);
+  width: 100%;
+  outline: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:focus {
+    border-bottom: 1px solid black;
+  }
 `;
 
 const InputContent = styled.textarea`
-    height: 60vh;
-    border: none;
-    outline: none;
-    width: 100%;
-    resize: none;
-    cursor: pointer;
-    margin-top: 15px;
-    font-size: 16px;
-    line-height: 30px;
+  height: 60vh;
+  border: none;
+  outline: none;
+  width: 100%;
+  resize: none;
+  cursor: pointer;
+  margin-top: 15px;
+  font-size: 16px;
+  line-height: 30px;
 `;
 const WriteTop = styled.div`
-    display: "flex";
-    flexdirection: "column";
+  display: "flex";
+  flexdirection: "column";
 `;
 
 const BtnContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const SecondStepHeader = styled.div`
-    font-size: 32px;
-    margin-bottom: 100px;
+  font-size: 32px;
+  margin-bottom: 100px;
 `;
 
 const DateInput = styled.input`
-    width: 100%;
-    height: 50px;
-    border: none;
-    outline: none;
-    border-bottom: 1px solid var(--gray02);
-    font-size: 24px;
-    margin-bottom: 675px;
-    cursor: pointer;
+  width: 100%;
+  height: 50px;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid var(--gray02);
+  font-size: 24px;
+  margin-bottom: 675px;
+  cursor: pointer;
 `;
 
 const Write = () => {
-    useEffect(() => {
-        if (!localStorage.getItem("accessToken")) {
-            router.push("/login");
-        }
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      router.push("/login");
+    }
 
-        client.defaults.headers[
-            "Authorization"
-        ] = `Bearer ${localStorage.getItem("accessToken")}`;
-    }, []);
-    const router = useRouter();
-    let [inputTitle, setInputTitle] = useState("");
-    let [inputContent, setInputContent] = useState("");
-    let [inputDate, setInputDate] = useState("");
+    client.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
+      "accessToken"
+    )}`;
+  }, []);
+  const router = useRouter();
+  let [inputTitle, setInputTitle] = useState("");
+  let [inputContent, setInputContent] = useState("");
+  let [inputDate, setInputDate] = useState("");
 
-    const [userData, setUserData] = useRecoilState(userDataState);
+  const [userData, setUserData] = useRecoilState(userDataState);
 
-    const handleTitleInput = (e) => {
-        setInputTitle(e.target.value);
+  const handleTitleInput = (e) => {
+    setInputTitle(e.target.value);
+  };
+  const handleContentInput = (e) => {
+    setInputContent(e.target.value);
+  };
+  const handleDateInput = (e) => {
+    setInputDate(e.target.value);
+  };
+
+  const handleNextStep = () => {
+    if (inputTitle === "" || inputContent === "") {
+      alert("모든 항목을 입력해주세요.");
+      return;
+    }
+    let temp = {
+      ...userData,
+      currentStep: true,
     };
-    const handleContentInput = (e) => {
-        setInputContent(e.target.value);
-    };
-    const handleDateInput = (e) => {
-        setInputDate(e.target.value);
-    };
+    setUserData(temp);
+  };
 
-    const handleNextStep = () => {
-        if (inputTitle === "" || inputContent === "") {
-            alert("모든 항목을 입력해주세요.");
-            return;
-        }
-        let temp = {
-            ...userData,
-            currentStep: true,
-        };
-        setUserData(temp);
-    };
+  const handleSubmit = () => {
+    if (inputDate === "") {
+      alert("모든 항목을 입력해주세요.");
+      return;
+    }
 
-    const handleSubmit = () => {
-        if (inputDate === "") {
-            alert("모든 항목을 입력해주세요.");
-            return;
-        }
+    client
+      .post("/api/letter", {
+        title: inputTitle,
+        content: inputContent,
+        receiveDate: `${inputDate}T00:00:00`,
+      })
+      .then((res) => {
+        console.log(res);
+        router.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log("submit");
+  };
 
-        client
-            .post("/api/letter", {
-                title: inputTitle,
-                content: inputContent,
-                receiveDate: `${inputDate}T00:00:00`,
-            })
-            .then((res) => {
-                console.log(res);
-                router.push("/");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        console.log("submit");
-    };
+  return (
+    <>
+      <HeadMeta title="편지쓰기" />
+      <PageWrapper>
+        <Header />
 
-    return (
-        <PageWrapper>
-            <Header />
+        {!userData.currentStep ? (
+          <>
+            <Contents>
+              <WriteTop>
+                <DateText>2022.10.16</DateText>
 
-            {!userData.currentStep ? (
-                <>
-                    <Contents>
-                        <WriteTop>
-                            <DateText>2022.10.16</DateText>
+                <InputTitle
+                  type="text"
+                  onChange={handleTitleInput}
+                  placeholder="제목을 입력해주세요"
+                  value={inputTitle}
+                ></InputTitle>
+              </WriteTop>
 
-                            <InputTitle
-                                type="text"
-                                onChange={handleTitleInput}
-                                placeholder="제목을 입력해주세요"
-                                value={inputTitle}
-                            ></InputTitle>
-                        </WriteTop>
-
-                        <InputContent
-                            onChange={handleContentInput}
-                            placeholder="당신의 이야기를 들려주세요"
-                            value={inputContent}
-                        ></InputContent>
-                    </Contents>
-                    <BtnContainer>
-                        <Modal title={inputTitle} content={inputContent} />
-                        <SmallFill onClick={handleNextStep}>다음</SmallFill>
-                    </BtnContainer>
-                </>
-            ) : (
-                <>
-                    <Contents
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <SecondStepHeader>받는 날짜 설정</SecondStepHeader>
-                        <DateInput
-                            type="date"
-                            value={inputDate}
-                            onChange={handleDateInput}
-                        />
-                        <LargeButton onClick={handleSubmit}>완료</LargeButton>
-                    </Contents>
-                </>
-            )}
-        </PageWrapper>
-    );
+              <InputContent
+                onChange={handleContentInput}
+                placeholder="당신의 이야기를 들려주세요"
+                value={inputContent}
+              ></InputContent>
+            </Contents>
+            <BtnContainer>
+              <Modal title={inputTitle} content={inputContent} />
+              <SmallFill onClick={handleNextStep}>다음</SmallFill>
+            </BtnContainer>
+          </>
+        ) : (
+          <>
+            <Contents
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <SecondStepHeader>받는 날짜 설정</SecondStepHeader>
+              <DateInput
+                type="date"
+                value={inputDate}
+                onChange={handleDateInput}
+              />
+              <LargeButton onClick={handleSubmit}>완료</LargeButton>
+            </Contents>
+          </>
+        )}
+      </PageWrapper>
+    </>
+  );
 };
 
 export default Write;
