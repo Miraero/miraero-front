67f56fc8c3a65e24c536c84cd4676cd502f08521
wugusaud77 <wugusaud77@icloud.com>
2022-10-17 21:42:18 +0900

    fix:conflict"

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import {
-  PageWrapper,
-  VerticalLeft,
-  DateText,
-  Welcome,
-  LargeButton,
+    PageWrapper,
+    VerticalLeft,
+    DateText,
+    Welcome,
+    LargeButton,
 } from "../components/Atomic";
 import styled from "@emotion/styled";
 import LetterBox from "../components/LetterBox";
@@ -20,17 +20,16 @@ const Home = () => {
     const date = new Date();
     const router = useRouter();
 
-  const [name, setName] = useState("");
-  useEffect(() => {
-    if (!localStorage.getItem("accessToken")) {
-      router.push("/login");
-    }
-    client.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
-      "accessToken"
-    )}`;
-    setName(localStorage.getItem("name"));
-  }, []);
-
+    const [name, setName] = useState("");
+    useEffect(() => {
+        if (!localStorage.getItem("accessToken")) {
+            router.push("/login");
+        }
+        client.defaults.headers[
+            "Authorization"
+        ] = `Bearer ${localStorage.getItem("accessToken")}`;
+        setName(localStorage.getItem("name"));
+    }, []);
 
     const handleWrite = (e) => {
         e.preventDefault();
@@ -42,121 +41,121 @@ const Home = () => {
         router.push("/write");
     };
 
-  const getLetters = async () => {
-    const { data } = await client.get("/api/letters");
-    return data.letters;
-  };
+    const getLetters = async () => {
+        const { data } = await client.get("/api/letters");
+        return data.letters;
+    };
 
-  const { data: letters, isLoading } = useQuery(["letters"], getLetters, {});
+    const { data: letters, isLoading } = useQuery(["letters"], getLetters, {});
 
-  return (
-    <>
-      <HeadMeta />
-      <PageWrapper
-        style={{
-          justifyContent: "space-between",
-        }}
- 
-        >
-            <PageTop>
-                <VerticalLeft
-                    style={{
-                        marginTop: "101px",
-                        gap: "3px",
-                    }}
-                >
-                    <DateText>
-                        {" "}
-                        {date.getFullYear()}.{date.getMonth() + 1}.
-                        {date.getDate()}
-                    </DateText>
-                    <Welcome>{name}님 안녕하세요.</Welcome>
-                </VerticalLeft>
-                <SectionTitle>받은 편지함</SectionTitle>
-                {isLoading ? (
-                    <NoLetter>
-                        <img
-                            src="/asset/noletter.png"
-                            alt="편지 없음 이미지"
-                            width={140}
-                        />
-                        <br />
-                        로딩중이에요!
-                        <br />
-                        잠시만 기다려주세요...
-                    </NoLetter>
-                ) : letters.length > 0 ? (
-                    <LetterView>
-                        {letters.map((letter) => (
-                            <LetterBox key={letter.id} letter={letter} />
-                        ))}
-                    </LetterView>
-                ) : (
-                    <NoLetter>
-                        <img
-                            src="/asset/noletter.png"
-                            alt="편지 없음 이미지"
-                            width={140}
-                        />
-                        <br />
-                        아직 날아온 편지가 없어요
-                        <br />
-                        자신에게 추억을 선물해주세요
-                    </NoLetter>
-                )}
-            </PageTop>
-            <PageEnd>
-                <LargeButton onClick={(e) => handleWrite(e)}>
-                    편지 쓰기
-                </LargeButton>
-                <div
-                    style={{
-                        height: "30px",
-                    }}
-                ></div>
-            </PageEnd>
-        </PageWrapper>
+    return (
+        <>
+            <HeadMeta />
+            <PageWrapper
+                style={{
+                    justifyContent: "space-between",
+                }}
+            >
+                <PageTop>
+                    <VerticalLeft
+                        style={{
+                            marginTop: "101px",
+                            gap: "3px",
+                        }}
+                    >
+                        <DateText>
+                            {" "}
+                            {date.getFullYear()}.{date.getMonth() + 1}.
+                            {date.getDate()}
+                        </DateText>
+                        <Welcome>{name}님 안녕하세요.</Welcome>
+                    </VerticalLeft>
+                    <SectionTitle>받은 편지함</SectionTitle>
+                    {isLoading ? (
+                        <NoLetter>
+                            <img
+                                src="/asset/noletter.png"
+                                alt="편지 없음 이미지"
+                                width={140}
+                            />
+                            <br />
+                            로딩중이에요!
+                            <br />
+                            잠시만 기다려주세요...
+                        </NoLetter>
+                    ) : letters.length > 0 ? (
+                        <LetterView>
+                            {letters.map((letter) => (
+                                <LetterBox key={letter.id} letter={letter} />
+                            ))}
+                        </LetterView>
+                    ) : (
+                        <NoLetter>
+                            <img
+                                src="/asset/noletter.png"
+                                alt="편지 없음 이미지"
+                                width={140}
+                            />
+                            <br />
+                            아직 날아온 편지가 없어요
+                            <br />
+                            자신에게 추억을 선물해주세요
+                        </NoLetter>
+                    )}
+                </PageTop>
+                <PageEnd>
+                    <LargeButton onClick={(e) => handleWrite(e)}>
+                        편지 쓰기
+                    </LargeButton>
+                    <div
+                        style={{
+                            height: "30px",
+                        }}
+                    ></div>
+                </PageEnd>
+            </PageWrapper>
+        </>
     );
 };
 
 const PageEnd = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const PageTop = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: auto;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: auto;
 `;
 
 const LetterView = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  max-height: calc(100vh - 330px);
-  overflow: scroll;
-  margin-top: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    max-height: calc(100vh - 330px);
+    overflow: scroll;
+    margin-top: 20px;
 `;
 
 const NoLetter = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: calc(100vh - 360px);
-  font-size: 16px;
-  color: var(--gray02);
-  text-align: center;
-  line-height: 23px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 360px);
+    font-size: 16px;
+    color: var(--gray02);
+    text-align: center;
+    line-height: 23px;
 `;
 const SectionTitle = styled.div`
-  margin-top: 38px;
-  font-weight: 500;
-  font-size: 16px;
-  color: var(--textColor);
+    margin-top: 38px;
+    font-weight: 500;
+    font-size: 16px;
+    color: var(--textColor);
 `;
 
 export default Home;
--- pages/write.js
+++ pages/write.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import {
-  DateText,
-  SmallFill,
-  PageWrapper,
-  LargeButton,
+    DateText,
+    SmallFill,
+    PageWrapper,
+    LargeButton,
 } from "../components/Atomic";
 import { Modal } from "../components/Modal";
 import styled from "@emotion/styled";
@@ -15,46 +15,44 @@ import { useRouter } from "next/router";
 import HeadMeta from "../components/HeadMeta";
 
 const Contents = styled.div`
-  margin-top: 102px;
+    margin-top: 102px;
 `;
 
 const InputTitle = styled.input`
-  padding: 10px 0px;
-  margin: 10px 0px;
-  border: none;
-  font-size: 24px;
-  border-bottom: 1px solid var(--gray02);
-  width: 100%;
-  outline: none;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  &:focus {
-    border-bottom: 1px solid black;
-  }
+    padding: 10px 0px;
+    margin: 10px 0px;
+    border: none;
+    font-size: 24px;
+    border-bottom: 1px solid var(--gray02);
+    width: 100%;
+    outline: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    &:focus {
+        border-bottom: 1px solid black;
+    }
 `;
 
 const InputContent = styled.textarea`
-  height: 60vh;
-  border: none;
-  outline: none;
-  width: 100%;
-  resize: none;
-  cursor: pointer;
-  margin-top: 15px;
-  font-size: 16px;
-  line-height: 30px;
+    height: 60vh;
+    border: none;
+    outline: none;
+    width: 100%;
+    resize: none;
+    cursor: pointer;
+    margin-top: 15px;
+    font-size: 16px;
+    line-height: 30px;
 `;
 const WriteTop = styled.div`
-
     display: "flex";
     flex-direction: "column";
-
 `;
 
 const BtnContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 `;
 
 const SecondStepHeader = styled.div`
@@ -116,7 +114,6 @@ const Write = () => {
     const [day, setDay] = useState(date.getDate());
     let [inputTitle, setInputTitle] = useState("");
     let [inputContent, setInputContent] = useState("");
-    let [inputDate, setInputDate] = useState("");
 
     const handleYear = (e) => {
         setYear(e.target.value);
@@ -133,45 +130,22 @@ const Write = () => {
     const handleTitleInput = (e) => {
         setInputTitle(e.target.value);
     };
+
     const handleContentInput = (e) => {
         setInputContent(e.target.value);
     };
-    const handleDateInput = (e) => {
-        setInputDate(e.target.value);
+
+    const handleNextStep = () => {
+        if (inputTitle === "" || inputContent === "") {
+            alert("모든 항목을 입력해주세요.");
+            return;
+        }
+        let temp = {
+            ...userData,
+            currentStep: true,
+        };
+        setUserData(temp);
     };
-
-    client.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
-      "accessToken"
-    )}`;
-  }, []);
-  const router = useRouter();
-  let [inputTitle, setInputTitle] = useState("");
-  let [inputContent, setInputContent] = useState("");
-  let [inputDate, setInputDate] = useState("");
-
-  const [userData, setUserData] = useRecoilState(userDataState);
-
-  const handleTitleInput = (e) => {
-    setInputTitle(e.target.value);
-  };
-  const handleContentInput = (e) => {
-    setInputContent(e.target.value);
-  };
-  const handleDateInput = (e) => {
-    setInputDate(e.target.value);
-  };
-
-  const handleNextStep = () => {
-    if (inputTitle === "" || inputContent === "") {
-      alert("모든 항목을 입력해주세요.");
-      return;
-    }
-    let temp = {
-      ...userData,
-      currentStep: true,
-    };
-    setUserData(temp);
-  };
 
     const handleSubmit = () => {
         if (year === "" || month === "" || day === "") {
@@ -204,107 +178,93 @@ const Write = () => {
         console.log("submit");
     };
 
-    client
-      .post("/api/letter", {
-        title: inputTitle,
-        content: inputContent,
-        receiveDate: `${inputDate}T00:00:00`,
-      })
-      .then((res) => {
-        console.log(res);
-        router.push("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    console.log("submit");
-  };
+    return (
+        <>
+            <HeadMeta title="편지쓰기" />
+            <PageWrapper
+                style={{
+                    justifyContent: "space-between",
+                }}
+            >
+                <Header />
 
-  return (
-    <>
-      <HeadMeta title="편지쓰기" />
-           <PageWrapper
-            style={{
-                justifyContent: "space-between",
-            }}
-        >
-            <Header />
+                {!userData.currentStep ? (
+                    <>
+                        <Contents>
+                            <WriteTop>
+                                <DateText>
+                                    {date.getFullYear()}.{date.getMonth() + 1}.
+                                    {date.getDate()}
+                                </DateText>
 
-            {!userData.currentStep ? (
-                <>
-                    <Contents>
-                        <WriteTop>
-                            <DateText>
-                                {date.getFullYear()}.{date.getMonth() + 1}.
-                                {date.getDate()}
-                            </DateText>
+                                <InputTitle
+                                    type="text"
+                                    onChange={handleTitleInput}
+                                    placeholder="제목을 입력해주세요"
+                                    value={inputTitle}
+                                ></InputTitle>
+                            </WriteTop>
 
-                            <InputTitle
-                                type="text"
-                                onChange={handleTitleInput}
-                                placeholder="제목을 입력해주세요"
-                                value={inputTitle}
-                            ></InputTitle>
-                        </WriteTop>
-
-                        <InputContent
-                            onChange={handleContentInput}
-                            placeholder="당신의 이야기를 들려주세요"
-                            value={inputContent}
-                        ></InputContent>
-                    </Contents>
-                    <BtnContainer>
-                        <Modal title={inputTitle} content={inputContent} />
-                        <SmallFill onClick={handleNextStep}>다음</SmallFill>
-                    </BtnContainer>
-                </>
-            ) : (
-                <>
-                    <Contents
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "space-between",
-                            minHeight: "100vh",
-                        }}
-                    >
-                        <SecondContainerTwo>
-                            <SecondStepHeader>
-                                언제 편지를 <br />
-                                받으실 건가요?
-                            </SecondStepHeader>
-                            <DateContainer>
-                                <DateItem>
-                                    <DateInput
-                                        type="number"
-                                        value={year}
-                                        onChange={handleYear}
-                                    />
-                                    <DateLabel>년</DateLabel>
-                                </DateItem>
-                                <DateItem>
-                                    <DateInput
-                                        type="number"
-                                        value={month}
-                                        onChange={handleMonth}
-                                    />
-                                    <DateLabel>월</DateLabel>
-                                </DateItem>
-                                <DateItem>
-                                    <DateInput
-                                        type="number"
-                                        value={day}
-                                        onChange={handleDay}
-                                    />
-                                    <DateLabel>일</DateLabel>
-                                </DateItem>
-                            </DateContainer>
-                        </SecondContainerTwo>
-                        <LargeButton onClick={handleSubmit}>완료</LargeButton>
-                    </Contents>
-                </>
-            )}
-        </PageWrapper>
+                            <InputContent
+                                onChange={handleContentInput}
+                                placeholder="당신의 이야기를 들려주세요"
+                                value={inputContent}
+                            ></InputContent>
+                        </Contents>
+                        <BtnContainer>
+                            <Modal title={inputTitle} content={inputContent} />
+                            <SmallFill onClick={handleNextStep}>다음</SmallFill>
+                        </BtnContainer>
+                    </>
+                ) : (
+                    <>
+                        <Contents
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "space-between",
+                                minHeight: "100vh",
+                            }}
+                        >
+                            <SecondContainerTwo>
+                                <SecondStepHeader>
+                                    언제 편지를 <br />
+                                    받으실 건가요?
+                                </SecondStepHeader>
+                                <DateContainer>
+                                    <DateItem>
+                                        <DateInput
+                                            type="number"
+                                            value={year}
+                                            onChange={handleYear}
+                                        />
+                                        <DateLabel>년</DateLabel>
+                                    </DateItem>
+                                    <DateItem>
+                                        <DateInput
+                                            type="number"
+                                            value={month}
+                                            onChange={handleMonth}
+                                        />
+                                        <DateLabel>월</DateLabel>
+                                    </DateItem>
+                                    <DateItem>
+                                        <DateInput
+                                            type="number"
+                                            value={day}
+                                            onChange={handleDay}
+                                        />
+                                        <DateLabel>일</DateLabel>
+                                    </DateItem>
+                                </DateContainer>
+                            </SecondContainerTwo>
+                            <LargeButton onClick={handleSubmit}>
+                                완료
+                            </LargeButton>
+                        </Contents>
+                    </>
+                )}
+            </PageWrapper>
         </>
     );
 };
